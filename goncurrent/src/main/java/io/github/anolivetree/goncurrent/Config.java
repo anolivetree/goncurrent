8e975bd05306bf3111c31c5a92934cd50f18bcf8
Hiroshi Sakurai <an.olive.tree@gmail.com>
2015-08-04 00:54:43 +0900

    DEBUG_CHECK_STATE to false

--- goncurrent/src/main/java/io/github/anolivetree/goncurrent/Config.java
+++ goncurrent/src/main/java/io/github/anolivetree/goncurrent/Config.java
@@ -17,5 +17,5 @@ package io.github.anolivetree.goncurrent;
 
 public class Config {
     static final boolean DEBUG_PRINT = false;
-    static final boolean DEBUG_CHECK_STATE = true;
+    static final boolean DEBUG_CHECK_STATE = false;
 }
